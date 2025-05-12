6963c64b0f0545ebd9f09a3e17cc340304ef7c9b
bananas869 <diogo_9819@hotmail.com>
2025-05-12 12:46:53 +0000

    modif faite

--- app-diogo.py
+++ app-diogo.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "wow salut youtube!! aaadqwqdqdddevqdqdqdq<sops" 
+    return "aurevoir youtube!! aaadqwqdqdddevqdqdqdq<sops" 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
